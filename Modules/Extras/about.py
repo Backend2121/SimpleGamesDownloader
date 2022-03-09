72d93011509b96a6515b788b07f9d8d5ec1dbc92
Alessandro Perini <2121masterhack2121@gmail.com>
2022-03-09 10:46:31 +0100

    Added BTC/ETH addresses

--- Modules/Extras/about.py
+++ Modules/Extras/about.py
@@ -51,10 +51,48 @@ class About(QWindow):
         self.youtubeButton.setIcon(QIcon(os.path.normpath(os.getcwd() + "/Icons/Youtube.png")))
         self.youtubeButton.setIconSize(QSize(20,20))
 
-        # BTC Address
+        # BTC Address & Icon
         self.BTCAddress = QLabel()
         self.BTCAddress.setAlignment(Qt.AlignCenter)
-        self.BTCAddress.setPixmap(QPixmap(os.path.normpath(os.getcwd() + "/Icons/PLEASECHANGEMEEEEEEE.png")))
+        self.BTCAddress.setPixmap(QPixmap(os.path.normpath(os.getcwd() + "/Icons/BTCAddress.png")).scaled(128, 128, Qt.KeepAspectRatio))
+
+        self.BTCIcon = QLabel()
+        self.BTCIcon.setAlignment(Qt.AlignCenter)
+        self.BTCIcon.setPixmap(QPixmap(os.path.normpath(os.getcwd() + "/Icons/BTC.png")).scaled(32, 32, Qt.KeepAspectRatio))
+
+        self.BTCLabel = QLabel()
+        self.BTCLabel.setText("bc1q0fuppgceyqlzhs9y4qyztqwaw40chd0rwlxf3q")
+        self.BTCLabel.setAlignment(Qt.AlignCenter)
+        self.BTCLabel.setTextInteractionFlags(Qt.TextSelectableByMouse)
+
+        self.BTCLayout = QVBoxLayout()
+        self.BTCLayout.addWidget(self.BTCIcon)
+        self.BTCLayout.addWidget(self.BTCAddress)
+        self.BTCLayout.addWidget(self.BTCLabel)
+
+        # ETH Address
+        self.ETHAddress = QLabel()
+        self.ETHAddress.setAlignment(Qt.AlignCenter)
+        self.ETHAddress.setPixmap(QPixmap(os.path.normpath(os.getcwd() + "/Icons/ETHAddress.png")).scaled(128, 128, Qt.KeepAspectRatio))
+
+        self.ETHIcon = QLabel()
+        self.ETHIcon.setAlignment(Qt.AlignCenter)
+        self.ETHIcon.setPixmap(QPixmap(os.path.normpath(os.getcwd() + "/Icons/ETH.png")).scaled(32, 32, Qt.KeepAspectRatio))
+
+        self.ETHLabel = QLabel()
+        self.ETHLabel.setText("0x04093b7d2B0698d3C3e61eC48702C070048614b7")
+        self.ETHLabel.setAlignment(Qt.AlignCenter)
+        self.ETHLabel.setTextInteractionFlags(Qt.TextSelectableByMouse)
+
+        self.ETHLayout = QVBoxLayout()
+        self.ETHLayout.addWidget(self.ETHIcon)
+        self.ETHLayout.addWidget(self.ETHAddress)
+        self.ETHLayout.addWidget(self.ETHLabel)
+
+        # Wallets layout
+        self.walletLayout = QHBoxLayout()
+        self.walletLayout.addLayout(self.BTCLayout)
+        self.walletLayout.addLayout(self.ETHLayout)
 
         # Add to mainLayout
         self.mainLayout.addWidget(self.titleLabel)
@@ -64,7 +102,7 @@ class About(QWindow):
         self.mainLayout.addWidget(self.redditButton)
         self.mainLayout.addWidget(self.patreonButton)
         self.mainLayout.addWidget(self.youtubeButton)
-        self.mainLayout.addWidget(self.BTCAddress)
+        self.mainLayout.addLayout(self.walletLayout)
 
         # Set main Layout
         self.widget.setLayout(self.mainLayout)
